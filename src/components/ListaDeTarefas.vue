178971d1bb2dd26463997e8ac492679678b15f6c
Thiago Batista <thiagobatista15390@gmail.com>
2025-02-02 12:16:27 -0300

    adicionando os componentes a aplicação

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps(["tarefas"]);
+</script>
+
+<template>
+    <ul class="list-group mt-4">
+        <li class="list-group-item" v-for="tarefa in props.tarefas" :key="tarefa.titulo">
+            <input v-model="tarefa.finalizada" :id="tarefa.titulo" type="checkbox">
+            <label :class="{ feito: tarefa.finalizada }" class="ms-3" :for="tarefa.titulo">
+                {{ tarefa.titulo }}
+            </label>
+        </li>
+    </ul>
+</template>
+
+<style scoped>
+.feito {
+    text-decoration: line-through;
+}
+</style>
